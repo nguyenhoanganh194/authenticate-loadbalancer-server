466d7ee58c25ae7b246d82060e61a57ef7eed95e
Anh Nguyen <nguyenhoanganha1y@gmail.com>
2023-02-23 16:06:19 +0200

    Update game server

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { encryptText, decryptText, getPublicKey } from './encrypt.js';
 import express, { json, urlencoded } from 'express';
+import http from 'http';
+import request from 'express'
+import axios from 'axios'
 const app = express();
-const port = 8080;
+const port = 8001;
 
 app.use(json())
 app.use(urlencoded({ extended: true }))
@@ -21,14 +24,24 @@ app.post("/login",(request, response) => {
   const decryptedText = decryptText(request.body.data)
   var loginData = JSON.parse(decryptedText);
   //TODO: data checking here
-  var ticket = {
-    "token": "123",
-    "username": loginData.username,
-    "time": Date.now(),
-  }
-  console.log(ticket);
-  var encryptedTextRes = encryptText(JSON.stringify(ticket),request.body.publickey);
-  response.send(encryptedTextRes.toString());
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'valid_token'
+  };
+  axios.post('http://127.0.0.1:9000/ticket', {
+    username: loginData.username,
+  }, { headers })
+  .then((serverRespond) => {
+    console.log("Receive ticket");
+    var ticket = JSON.stringify(serverRespond.data);
+    var encrypt = encryptText(ticket,request.body.publickey);
+    response.send(encrypt)
+  })
+  .catch((error) => {
+    console.log(error);
+    var encrypt = encryptText("False",request.body.publickey);
+    response.send(encrypt)
+  });
 });
 
 
